b90007daf453cf31c5473dcfed4c19abdf19b834
lanniiee <lav250397@hotmail.com>
2022-11-16 14:46:33 +0000

    add in conditions to check for previous numbers

--- src/main/java/com/nology/numbergame/NumberGame.java
+++ src/main/java/com/nology/numbergame/NumberGame.java
@@ -1,4 +1,5 @@
 package com.nology.numbergame;
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.Random;
 
@@ -6,49 +7,67 @@ public class NumberGame {
     public static void main(String[] args) {
         Scanner myScan = new Scanner(System.in);
 
-        int lives = 9;
+        int lives = 10;
 
         Random rand = new Random();
         int min = 1;
         int max = 100;
         int num = rand.nextInt((max - min) + 1) + min;
 
-        for (int i = 0; i <= 10 ; i++) {
+        ArrayList<Integer> guessedNumbers = new ArrayList<>();
+
+        for (int i = 10; i > 0 ; i--) {
             System.out.println("Enter your guess");
             int userGuess = myScan.nextInt();
             int difference = Math.abs(userGuess - num);
-            System.out.println("lives remaining: " + lives);
-            if (userGuess == num) {
-                System.out.println("You have guessed correctly!");
-                System.out.println("Actual number: " + num);
-                i=10;
-            } else if (difference <= 3) {
-                System.out.println("Very very warm");
-                lives -= 1;
-            } else if (difference <= 6) {
-                System.out.println("Very warm");
-                lives -= 1;
-            } else if (difference <= 10) {
-                System.out.println("Warm");
-                lives -= 1;
-            } else if (difference <= 16) {
-                System.out.println("Cold");
-                lives -= 1;
-            } else if (difference <= 21) {
-                System.out.println("Very Cold");
-                lives -= 1;
-            } else if (difference <= 25) {
-                System.out.println("Freezing");
-                lives -= 1;
-            } else if (difference <= 30) {
-                System.out.println("Artic");
-                lives -= 1;
+
+            if (!guessedNumbers.contains(userGuess)) {
+                guessedNumbers.add(userGuess);
+                if (userGuess == num) {
+                    System.out.println("You have guessed correctly!");
+                    System.out.println("Actual number: " + num);
+                    i = 0;
+                } else if (difference <= 3) {
+                    lives -= 1;
+                    System.out.println("Very very warm");
+                    System.out.println("lives remaining: " + lives);
+                } else if (difference <= 6) {
+                    lives -= 1;
+                    System.out.println("Very warm");
+                    System.out.println("lives remaining: " + lives);
+                } else if (difference <= 10) {
+                    lives -= 1;
+                    System.out.println("Warm");
+                    System.out.println("lives remaining: " + lives);
+                } else if (difference <= 16) {
+                    lives -= 1;
+                    System.out.println("Cold");
+                    System.out.println("lives remaining: " + lives);
+                } else if (difference <= 21) {
+                    lives -= 1;
+                    System.out.println("Very Cold");
+                    System.out.println("lives remaining: " + lives);
+                } else if (difference <= 25) {
+                    lives -= 1;
+                    System.out.println("Freezing");
+                    System.out.println("lives remaining: " + lives);
+                } else if (difference <= 30) {
+                    lives -= 1;
+                    System.out.println("Artic");
+                    System.out.println("lives remaining: " + lives);
+                }
+                else {
+                    lives -= 1;
+                    System.out.println("No where near..");
+                    System.out.println("lives remaining: " + lives);
+                }
+                if (lives == 0) {
+                    System.out.println("Game over! The number was " + num);
+                }
             }
             else {
-                System.out.println("No where near..");
-                lives -= 1;
+                System.out.println("You have already used this number! Try Again.");
             }
         }
-
     }
 }
